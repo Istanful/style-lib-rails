4aa4c5aa23258a126b6928f43e2fccda51ceeec7
Emric <emric.mansson@gmail.com>
2021-06-13 13:12:06 +0200

    Implement stack
    A stack distributes items vertically. This commit implements and
    documents the stack.

--- lib/style/lib/rails/engine.rb
+++ lib/style/lib/rails/engine.rb
@@ -1,8 +1,16 @@
+require 'sassc-rails'
+
 module Style
   module Lib
     module Rails
       class Engine < ::Rails::Engine
         isolate_namespace Style::Lib::Rails
+
+        initializer 'style-lib-rails.assets' do |app|
+          %w(stylesheets).each do |sub|
+            app.config.assets.paths << root.join('assets', sub).to_s
+          end
+        end
       end
     end
   end
